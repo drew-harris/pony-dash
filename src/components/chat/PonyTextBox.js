7331350e91847942a168348be68b727dfe4f6846
Eli <elisego46@gmail.com>
2021-11-08 13:18:15 -0600

    Add simple textbox for chat * Add text area for user to type message * Add button for user to submit message to firestore * Push to 'messages' collection upon submitting

---
+++ src/components/chat/PonyTextBox.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../../firebase';
+
+function PonyTextBox({ user }) {
+    const [textInfo, setTextInfo] = useState([
+        {
+            text: '',
+            user,
+        },
+    ]);
+
+    async function handleClick(e) {
+        if (textInfo.text.trim()) {
+            setTextInfo({ user, text: '' });
+            await addDoc(collection(db, 'messages'), textInfo);
+        } else {
+            alert('empty text');
+            setTextInfo({ user, text: '' });
+        }
+    }
+
+    return (
+        <div>
+            <textarea
+                onChange={e => setTextInfo({ user, text: e.target.value })}
+                value={textInfo.text}
+                name='chat'
+                id='this'
+                cols='29'
+                rows='10'
+                placeholder='Send a Message'
+            ></textarea>
+            <button onClick={handleClick}>Click this damn button</button>
+        </div>
+    );
+}
+
+export default PonyTextBox;
